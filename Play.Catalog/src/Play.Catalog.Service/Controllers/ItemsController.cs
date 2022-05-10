f0a59e5a51ad0b91306f783abf82009e69c609f2
haitc <hai.tc21@gmail.com>
2022-05-10 16:29:31 +0700

    Tao db mongo

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -9,12 +9,6 @@ namespace Play.Catalog.Service.Controllers;
 public class ItemsController : ControllerBase
 {
     private readonly ItemsRepository _itemsRepository = new();
-    private static List<ItemDto> items = new()
-        {
-            new ItemDto(Guid.NewGuid(),"Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
-            new ItemDto(Guid.NewGuid(),"AntiDote", "Cures poisn", 7, DateTimeOffset.UtcNow),
-            new ItemDto(Guid.NewGuid(),"Bronze sword", "Restores a small amount of damage", 20, DateTimeOffset.UtcNow)
-        };
 
     [HttpGet]
     public async Task<IEnumerable<ItemDto>> GetAsync()
@@ -36,14 +30,14 @@ public class ItemsController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<ItemDto>> PostAsync(CreateItemDto createItemDto)
     {
+
         var item = new Item
         {
             Id = Guid.NewGuid(),
-
             Name = createItemDto.Name,
             Description = createItemDto.Description,
             Price = createItemDto.Price,
-            CreatedDate = DateTimeOffset.UtcNow
+            CreatedDate = DateTimeOffset.Now
         };
         await _itemsRepository.CreateAsync(item);
         return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
--- Play.Catalog/src/Play.Catalog.Service/Dtos.cs
+++ Play.Catalog/src/Play.Catalog.Service/Dtos.cs
@@ -3,7 +3,8 @@ using System.ComponentModel.DataAnnotations;
 namespace Play.Catalog.Service.Dtos
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-    public record CreateItemDto([Required] string Name, string Description, [Range(0, 100)] decimal Price);
-    public record UpdateIItemDto([Required] string Name, string Description, [Range(0, 100)] decimal Price);
 
+    public record CreateItemDto([Required] string Name, string Description, [Range(0, 100)] decimal Price);
+
+    public record UpdateIItemDto([Required] string Name, string Description, [Range(0, 100)] decimal Price);
 }
